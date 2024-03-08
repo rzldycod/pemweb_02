18132d5d130c95bbeea6fec4d854181905295945
Miftahul Jannah <miftahul.jannah@microfocus.com>
2024-03-08 10:37:28 +0700

    praktikum 3 - Skills

---
+++ Praktikum 3/proses_registrasi.php
@@ -0,0 +1,13 @@
+<?php
+// Array Numerik - Index & Value
+$domisili = ["Jakarta","Bogor","Depok","Tangerang","Bekasi"];
+
+
+// Array Assosiative - Key & Value
+$prodi = [
+    "SI" => "Sistem Informasi",
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
+
+?>
