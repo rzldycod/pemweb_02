4c39cee0234cb5cd421134efad843de7133bae87
Miftahul Jannah <miftahul.jannah@microfocus.com>
2024-03-15 10:52:12 +0700

    Praktikum 4 & Tugas

---
+++ Praktikum 4/Admin/praktikum1.php
@@ -0,0 +1,18 @@
+<?php 
+require_once 'navbar.html'; 
+require_once 'sidebar.html'; 
+?>
+
+<h1>Praktikum 1</h1>
+<?php 
+$fruits = ["Bananas","Avocados","Watermelons","Starfruits"]; 
+
+echo "<ol>";
+foreach ($fruits as $fruit){
+    echo "<li>" . $fruit . "</li>";
+}
+echo "</ol>";
+
+?>
+
+<?php require_once 'footer.html'; ?>
