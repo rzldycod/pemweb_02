0dd1133c5aa9409b4329e2f3e17ec0dbe1909099
Miftahul Jannah <miftahul.jannah@microfocus.com>
2024-06-14 10:39:09 +0700

    Menambah Praktikum 14 - Laravel Breeze

--- puskesmas-new/resources/views/components/navbar.blade.php
+++ puskesmas-new/resources/views/components/navbar.blade.php
@@ -39,9 +39,24 @@
         </div>
       </li>
 
+      <li class="nav-item dropdown">
+        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }}
+        </a>
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                {{ __('Logout') }}
+            </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                @csrf
+            </form>
+        </div>
+      </li>
       <!-- Messages Dropdown Menu -->
       <!-- Notifications Dropdown Menu -->
-      <!-- <li class="nav-item dropdown">
+      <li class="nav-item dropdown">
         <a class="nav-link" data-toggle="dropdown" href="#">
           <i class="far fa-bell"></i>
           <span class="badge badge-warning navbar-badge">15</span>
@@ -66,7 +81,7 @@
           <div class="dropdown-divider"></div>
           <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
         </div>
-      </li> -->
+      </li>
       <li class="nav-item">
         <a class="nav-link" data-widget="fullscreen" href="#" role="button">
           <i class="fas fa-expand-arrows-alt"></i>
--- puskesmas-new/routes/web.php
+++ puskesmas-new/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
@@ -10,6 +11,39 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('dashboard')->group(function () {
+        Route::get('/', [AdminController::class, 'index']);
+        // Pasien
+        Route::prefix('pasien')->group(function () {
+            Route::get('/', [PasienController::class, 'index']);
+            Route::get('/create', [PasienController::class, 'create']);
+            Route::post('/store', [PasienController::class, 'store']);
+            Route::get('/show/{id}', [PasienController::class, 'show']);
+
+        });
+        // Kelurahan
+        Route::prefix('kelurahan')->group(function () {
+            Route::get('/', [KelurahanController::class, 'index']);
+            Route::get('/create', [KelurahanController::class, 'create']);
+            Route::post('/store', [KelurahanController::class, 'store']);
+            Route::get('/show/{id}', [KelurahanController::class, 'show']);
+            Route::get('/edit/{id}', [KelurahanController::class, 'edit']);
+            Route::put('/update/{id}', [KelurahanController::class, 'update']);
+            Route::delete('/destroy/{id}', [KelurahanController::class, 'destroy']);
+        });
+    });
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/beranda', function () {
     return "Halaman Beranda";
@@ -25,26 +59,26 @@ Route::get('/profile', function () {
 
 // Praktikum Laravel 2
 
-Route::get('/dashboard', [AdminController::class, 'index']);
+// Route::get('/dashboard', [AdminController::class, 'index']);
 Route::get('/contact', [ContactController::class, 'index']);
 Route::get('/about', [AboutController::class, 'index']);
 
 // Praktikum Laravel 3
-Route::get('/dashboard/kelurahan', [KelurahanController::class, 'index']);
-Route::get('/dashboard/pasien', [PasienController::class, 'index']);
+// Route::get('/dashboard/kelurahan', [KelurahanController::class, 'index']);
+// Route::get('/dashboard/pasien', [PasienController::class, 'index']);
 
 
 // Praktikum Laravel 4
-Route::get('/dashboard/kelurahan/create', [KelurahanController::class, 'create']);
-Route::post('/dashboard/kelurahan/store', [KelurahanController::class, 'store']);
-Route::get('/dashboard/kelurahan/show/{id}', [KelurahanController::class, 'show']);
+// Route::get('/dashboard/kelurahan/create', [KelurahanController::class, 'create']);
+// Route::post('/dashboard/kelurahan/store', [KelurahanController::class, 'store']);
+// Route::get('/dashboard/kelurahan/show/{id}', [KelurahanController::class, 'show']);
 
 // Praktikum Laravel 4
-Route::get('/dashboard/pasien/create', [PasienController::class, 'create']);
-Route::post('/dashboard/pasien/store', [PasienController::class, 'store']);
-Route::get('/dashboard/pasien/show/{id}', [PasienController::class, 'show']);
+// Route::get('/dashboard/pasien/create', [PasienController::class, 'create']);
+// Route::post('/dashboard/pasien/store', [PasienController::class, 'store']);
+// Route::get('/dashboard/pasien/show/{id}', [PasienController::class, 'show']);
 
 // Praktikum Laravel 5
-Route::get('/dashboard/kelurahan/edit/{id}', [KelurahanController::class, 'edit']);
-Route::put('/dashboard/kelurahan/update/{id}', [KelurahanController::class, 'update']);
-Route::delete('/dashboard/kelurahan/destroy/{id}', [KelurahanController::class, 'destroy']);
+// Route::get('/dashboard/kelurahan/edit/{id}', [KelurahanController::class, 'edit']);
+// Route::put('/dashboard/kelurahan/update/{id}', [KelurahanController::class, 'update']);
+// Route::delete('/dashboard/kelurahan/destroy/{id}', [KelurahanController::class, 'destroy']);
